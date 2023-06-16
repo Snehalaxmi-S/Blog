54071e8f2d628c18164f3d06fd766ccff1a9f358
Snehalaxmi-S <snehalaxmi2104@gmail.com>
2023-06-16 22:50:29 +0530

    Updated top

--- my-app/src/Top/TopBar.jsx
+++ my-app/src/Top/TopBar.jsx
@@ -1,11 +1,28 @@
 import './topstyle.css';
 
-const TopBar = () => {
+export default function TopBar() {
   return (
-    <div>
-      <h1>TopBar</h1>
+    <div className='topi'>
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img className="topImg" src="C:\Users\Welcome\Documents\React Blog\my-app\src\images\user.png" alt="" />
+        <i className="topSearch fas fa-search"></i>
+      </div>
     </div>
   )
 }
-
-export default TopBar
