a3100c7d033b5f1c8798ab97424b619222122987
Snehalaxmi-S <snehalaxmi2104@gmail.com>
2023-06-18 21:40:18 +0530

    Updated settings

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
 import TopBar from "./components/Top/TopBar";
+import Settings from "./pages/settings/Settings";
 // import Home from "./pages/home/Home";
 // import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
+// import Write from "./pages/write/Write";
 
 
 function App() {
   return (
     <div className="App">
     <TopBar />
-    <Write />
+    <Settings />
       
     </div>
   );
